ce20a24359369923a296d04e36730c0ec070175c
github username <github email address>
2019-09-19 17:41:22 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+require'pry'
+class Dog
+    attr_reader :name
+    @@all=[]
+
+
+def save
+    @@all<<self
+end
+def initialize(name)
+    @name=name
+self.save
+end
+
+
+def self.all
+@@all
+end
+
+def  self.clear_all
+@@all.length.times{@@all.pop}
+end
+
+def self.print_all
+@@all.each do |element|
+    puts element.name
+end
+end
+
+
+end
+
+d1=Dog.new("hhh")
+Dog.print_all
