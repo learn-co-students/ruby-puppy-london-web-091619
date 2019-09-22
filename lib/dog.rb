fa6ea1d19e91364d21d406ff2bc9da26f44ce4cf
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-22 16:41:01 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,54 @@
 # Add your code here
+class Dog
+    attr_reader :name
+    @@all = [] #This array is the storage container for each instance of a Dog that gets created
+
+    def initialize(name)
+        @name = name
+        save
+        # save
+    end
+    def self.all
+        @@all
+      end
+    
+      def self.print_all
+        
+        puts @@all.map{ |dog| dog.name }
+      end
+    
+      def self.clear_all
+        @@all.clear
+      end
+    
+      def save
+        @@all << self
+      end
+
+    # def self.all
+    #     @@all
+    # end
+
+    # def self.clear_all
+    #     @@all.clear 
+    # end
+
+    # def self.print_all
+    #     puts @@all.map{ |dog| dog.name }
+    # end
+    
+
+    # def save
+    #     @@all << name
+    # end
+
+   
+
+   
+
+   
+
+
+
+
+end
